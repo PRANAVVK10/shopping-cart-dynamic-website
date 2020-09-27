2fb040a01334f21a50bcad66e9cce92a6897a9bb
pranav v k <pranavvk34@gmail.com>
2020-09-26 22:08:02 -0700

    data access from db and show it to user

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 
 var app = express();
-var fileUpload= require('express-fileUpload')
+var fileUpload= require('express-fileUpload');
+var db = require('./config/connection');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -19,6 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+db.connect((err)=>{
+  if (err) console.log("connection failed"+err);
+  else console.log("database connected to port 27017");
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
---
+++ helpers/product-helpers.js
@@ -0,0 +1,17 @@
+var db=require("../config/connection")
+var collection=require("../config/collections")
+module.exports={
+    addProduct:(product,callback)=>{
+       
+        db.get().collection("product").insertOne(product).then((data)=>{
+            console.log(data);
+            callback(data.ops[0]._id)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let product= await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(product)
+        })
+    }
+}
