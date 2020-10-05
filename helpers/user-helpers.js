0154fede857a76a7d61aba653fbe26e020fa0436
pranav v k <pranavvk34@gmail.com>
2020-10-04 23:06:46 -0700

    login logout session added

---
+++ helpers/user-helpers.js
@@ -0,0 +1,38 @@
+var db=require("../config/connection")
+var collection=require("../config/collections")
+const bcrypt=require('bcrypt')
+module.exports={
+    doSignup:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            userData.password=await bcrypt.hash(userData.password,10)
+            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
+                resolve(data.ops[0])
+            })
+        })
+    
+    },
+    doLogin:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus=false
+            let response={}
+            let user=await db.get().collection(collection.USER_COLLECTION).findOne({email:userData.email})
+            if(user){
+                bcrypt.compare(userData.password,user.password).then((status)=>{
+                    if(status){
+                        console.log("success")
+                        response.user=user
+                        response.status=true
+                        resolve(response)
+                    }else{
+                        console.log("failed")
+                        resolve({status:false})
+                    }
+                })
+            }else{
+                console.log("faile")
+                resolve({status:false})
+            }
+        })
+    }
+}
+   
